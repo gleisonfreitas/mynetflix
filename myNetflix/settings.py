596570bed57847bd79a14451aebaf8a802462244
Gleison Freitas <gleison8405@gmail.com>
2023-12-30 20:11:18 -0300

    update config variable toke_csrf 2

--- myNetflix/settings.py
+++ myNetflix/settings.py
@@ -31,6 +31,7 @@ if TOKEN_CSRF:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ['mynetflix-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
